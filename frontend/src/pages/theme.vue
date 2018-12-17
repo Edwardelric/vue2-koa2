3437a148b2a41b1721347c950b3e94417ff4d3a9
Edward <838298008@qq.com>
2018-12-17 15:39:50 +0800

    添加render测试

---
+++ frontend/src/pages/theme.vue
@@ -0,0 +1,34 @@
+<script type="text/ecmascript-6">
+	export default {
+	  name: 'theme',
+    render(h) {
+
+      const theme = this.$attrs;
+      const merge = vNode => {
+        if (!vNode.tag) {
+          return;
+        }
+        if (vNode.componentOptions) {
+          let props = vNode.componentOptions.propsData;
+          props = Object.assign({}, theme, props);
+          vNode.componentOptions.propsData = props;
+        } else {
+          if (!vNode.data) {
+            return;
+          }
+          let attrs = vNode.data.attrs || {};
+          attrs = Object.assign({}, theme, attrs);
+          vNode.data.attrs = attrs;
+        }
+      };
+      this.$slots.default.map(vNode => merge(vNode));
+      Object.keys(this.$attrs).forEach(key => {
+        this.$attrs[key] = null;
+      })
+      return this.$slots.default[0]
+    }
+	};
+</script>
+
+<style lang="scss">
+</style>
---
+++ frontend/src/pages/themeContent.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    demo demo
+    {{color}}
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+	export default {
+	  props: ['color'],
+		data() {
+			return {};
+		},
+    created() {
+    }
+	};
+</script>
+
+<style lang="scss">
+</style>
