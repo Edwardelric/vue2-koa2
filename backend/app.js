118f5d11b735e6a744dea5d6f6c8752663f607be
Edward <838298008@qq.com>
2019-01-28 14:18:38 +0800

    [test]app.js

--- backend/app.js
+++ backend/app.js
@@ -3,26 +3,25 @@ const app = new koa();
 const Router = require('koa-router');
 const bodyParser = require('koa-bodyparser');
 const cors = require('koa2-cors');
-// const { connect, initSchema } = require('./database/init');
+const { connect, initSchema } = require('./database/init');
 
 app.use(bodyParser());
 app.use(cors());
 
 (async() => {
 	await connect();
-	//initSchema();
+	initSchema();
 })();
 
-// let user = require('./controller/user.js');
-// let goods = require('./controller/goods.js');
-//
-// let router = new Router();
-// router.use('/user', user.routes());
-// router.use('/goods', goods.routes());
-//
-// app.use(router.routes());
-// app.use(router.allowedMethods());
-//
-module.exports = app;
+let user = require('./controller/user.js');
+let goods = require('./controller/goods.js');
 
+let router = new Router();
+router.use('/user', user.routes());
+router.use('/goods', goods.routes());
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+module.exports = app;
 
