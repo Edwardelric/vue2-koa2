23f089598e65327091f4d3782531931e7e0a7ded
Edward <838298008@qq.com>
2019-06-04 17:48:03 +0800

    vue源码结构分析

--- frontend/src/testComponents/vue_source.vue
+++ frontend/src/testComponents/vue_source.vue
@@ -20,6 +20,30 @@
                 }
             }
             initMixin(Vue)
+
+            stateMixin(Vue)
+
+
+            Vue.prototype.$on = function() {
+
+            }
+            Vue.prototype.$emit = function() {
+
+            }
+            eventMixin(Vue)
+            
+
+            Vue.prototype._update = function(vnode) {
+
+            }
+            lifecycleMixin(Vue)
+
+            // compiler 编译成render函数， 返回虚拟dom 没有compiler只支持render写法
+            // core 核心
+            // platform 平台(web, weex)
+            // server 服务端渲染
+            // sfc  .vue 文件解析
+            // shared 公用的方法和常量
         </pre>
     </div>
 </template>
